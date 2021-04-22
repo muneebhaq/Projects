de5ff2853e2e6f74422ccb2dc5bbbf6dd80edf43
muneeb haq <muneeb.haq89@gmail.com>
2021-04-22 14:57:31 -0400

    new

--- software engineering/JS/stringproject.js
+++ software engineering/JS/stringproject.js
@@ -2,8 +2,16 @@ let city = "New York"
 let weather = "cold"
 let userInput = "   if you like cold weather than you should move to New York";
 let store = ""
-for(let counter = 0; counter < userInput.length; counter++){
-    store = store + userInput[counter].toUpperCase()
+for(let i = 0; i < userInput.length; i++){
+    store = store + userInput[i].toUpperCase()
+    
+    
 }
-let store1 = store.replace("New York",("Chicago"))
-console.log("  " + store1.trim());
+let store1 = userInput.replace("New York",("Chicago"))
+let store2 = store.concat(" else, stay in Florida enjoy the warm weather")
+let store3 = store.trim();
+console.log(store1);
+console.log(store2);
+console.log(store3);
+
+
